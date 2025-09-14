6919ac9a79d7ead8e58ec80dbcd9ca2343315f5c
ramesh metta <ramesh.metta@hotmail.com>
2025-09-14 01:01:04 -0500

    fixed

--- dist/service-worker.js
+++
@@ -1,8 +0,0 @@
-// Qwik Tools Service Worker
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  self.clients.claim();
-});
-// Add fetch, cache, or other logic as needed
